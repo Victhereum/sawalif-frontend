e9175d95405848171eaa08518edaacd8802d8181
EmmanuelChika <futureatpivot@gmail.com>
2024-12-09 03:02:06 +0100

    ✨feat(Constants): Add constant files and exported as needed

---
+++ constants/images.tsx
@@ -0,0 +1,56 @@
+import LogoIcon from "@/public/icons/logo.svg";
+import AppstoreIcon from "@/public/icons/appstore-icon.svg";
+import PlaystoreIcon from "@/public/icons/google-play-icon.svg";
+import VcIcon from "@/public/icons/vc-icon.svg";
+import CardBgIcon from "@/public/icons/cardbg-icon.svg";
+import AppIllustration from "@/public/icons/app-illustration.svg";
+import FacebookIcon from "@/public/icons/facebook-icon.svg";
+import LinkedInIcon from "@/public/icons/linkedIn-icon.svg";
+import XIcon from "@/public/icons/x-icon.svg";
+import GlodeIcon from "@/public/icons/globe.svg";
+import LogoLight from "@/public/icons/logo-light.svg";
+
+import Image01 from "@/public/icons/img01.svg";
+import Image02 from "@/public/icons/img02.svg";
+import Image03 from "@/public/icons/img03.svg";
+import Image04 from "@/public/icons/img04.svg";
+import Image05 from "@/public/icons/img05.svg";
+import Image06 from "@/public/icons/img06.svg";
+
+import Image07 from "@/public/icons/img07.svg";
+import Image08 from "@/public/icons/img08.svg";
+
+import Flag01 from "@/public/icons/flag01.svg";
+import Flag02 from "@/public/icons/flag02.svg";
+import Flag03 from "@/public/icons/flag03.svg";
+import Flag04 from "@/public/icons/flag04.svg";
+import Flag05 from "@/public/icons/flag05.svg";
+import Flag06 from "@/public/icons/flag06.svg";
+
+export {
+  LogoIcon,
+  AppstoreIcon,
+  PlaystoreIcon,
+  VcIcon,
+  CardBgIcon,
+  AppIllustration,
+  FacebookIcon,
+  LinkedInIcon,
+  XIcon,
+  LogoLight,
+  Image01,
+  Image02,
+  Image03,
+  Image04,
+  Image05,
+  Image06,
+  Image07,
+  Image08,
+  GlodeIcon,
+  Flag01,
+  Flag02,
+  Flag03,
+  Flag04,
+  Flag05,
+  Flag06,
+};
---
+++ constants/index.tsx
@@ -0,0 +1,51 @@
+import { ProfileCardProp } from "@/components/custom/profile-card";
+import {
+  Flag01,
+  Flag02,
+  Flag03,
+  Flag04,
+  Flag05,
+  Flag06,
+  Image01,
+  Image02,
+  Image03,
+  Image04,
+  Image05,
+  Image06,
+} from "./images";
+
+export const ProfileData01: ProfileCardProp[] = [
+  {
+    image: Image01,
+    name: "Williamson",
+    flag: Flag01,
+  },
+  {
+    image: Image02,
+    name: "Jane Cooper",
+    flag: Flag02,
+  },
+  {
+    image: Image03,
+    name: "Tiffany N.",
+    flag: Flag03,
+  },
+];
+
+export const ProfileData02: ProfileCardProp[] = [
+  {
+    image: Image04,
+    name: "Helen",
+    flag: Flag04,
+  },
+  {
+    image: Image05,
+    name: "Brooklyn",
+    flag: Flag05,
+  },
+  {
+    image: Image06,
+    name: "Jessica",
+    flag: Flag06,
+  },
+];
