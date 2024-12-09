cf4c1aa93015ddca5ed3af9dba93024dd9a31d46
EmmanuelChika <futureatpivot@gmail.com>
2024-12-09 03:04:14 +0100

    ✨feat(Sections): Add needed sections and components

---
+++ app/sections/index.tsx
@@ -0,0 +1,6 @@
+export { default as NavBar } from "./nav-bar";
+export { default as HeroSection } from "./hero";
+export { default as Section02 } from "./section02";
+export { default as Section03 } from "./section03";
+export { default as Section04 } from "./section04";
+export { default as Footer } from "./footer";
